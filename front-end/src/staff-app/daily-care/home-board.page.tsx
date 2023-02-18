88a74fbb70d49db87b9dbff6c8bf95fc96982897
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-02-18 09:10:59 +0530

    feat: UI markupfor sort selection

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import Button from "@material-ui/core/ButtonBase"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSortAlphaDown, faSortAlphaUp } from "@fortawesome/free-solid-svg-icons"
+
 import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
 import { Colors } from "shared/styles/colors"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
@@ -10,18 +12,31 @@ import { useApi } from "shared/hooks/use-api"
 import { StudentListTile } from "staff-app/components/student-list-tile/student-list-tile.component"
 import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active-roll-overlay/active-roll-overlay.component"
 
+const SORT_BY_OPTIONS = [
+  { label: "First name", value: "firstName" },
+  { label: "Last name", value: "lastName" },
+]
+
 export const HomeBoardPage: React.FC = () => {
   const [isRollMode, setIsRollMode] = useState(false)
+  const [sortBy, setSortBy] = useState<string>(SORT_BY_OPTIONS[0].value)
+  const [sortAsc, setSortAsc] = useState(true)
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
 
   useEffect(() => {
     void getStudents()
   }, [getStudents])
 
-  const onToolbarAction = (action: ToolbarAction) => {
+  const onToolbarAction = (action: ToolbarAction, value?: string) => {
     if (action === "roll") {
       setIsRollMode(true)
+    } else if (action === "sort" && value) {
+      handleSortByChange(value)
     }
+  }
+
+  const handleSortByChange = (value: string) => {
+    setSortBy(value)
   }
 
   const onActiveRollAction = (action: ActiveRollAction) => {
@@ -33,7 +48,7 @@ export const HomeBoardPage: React.FC = () => {
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} />
+        <Toolbar onItemClick={onToolbarAction} sortBy={sortBy} handleSortToggle={() => setSortAsc((prev: boolean) => !prev)} sortAsc={sortAsc} />
 
         {loadState === "loading" && (
           <CenteredContainer>
@@ -63,12 +78,25 @@ export const HomeBoardPage: React.FC = () => {
 type ToolbarAction = "roll" | "sort"
 interface ToolbarProps {
   onItemClick: (action: ToolbarAction, value?: string) => void
+  sortBy: string
+  sortAsc: boolean
+  handleSortToggle: () => void
 }
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
+  const { onItemClick, sortBy, sortAsc, handleSortToggle } = props
   return (
     <S.ToolbarContainer>
-      <div onClick={() => onItemClick("sort")}>First Name</div>
+      <div>
+        <label>Sort by:</label>
+        <select value={sortBy} onChange={(e) => onItemClick("sort", e.target.value)}>
+          {SORT_BY_OPTIONS.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleSortToggle}>{sortAsc ? <FontAwesomeIcon icon={faSortAlphaDown} /> : <FontAwesomeIcon icon={faSortAlphaUp} />}</button>
+      </div>
       <div>Search</div>
       <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
     </S.ToolbarContainer>
